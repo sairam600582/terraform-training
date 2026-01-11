b841ab9d289e66b327f409291911e73e01744142
Sairam Bisai <bisaisairam143p@gmail.com>
2026-01-11 13:01:50 +0530

    Add variables for region, profile, VPC, and subnet

---
+++ VPC/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "profile" {
+  default = "payal-1"
+}
+variable "vpc" {
+  default = "10.0.0.0/16"
+}
+
+variable "subent" {
+  default = "10.0.1.0/24"
+}
