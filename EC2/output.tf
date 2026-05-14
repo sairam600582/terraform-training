690d6ab552f80e8c0b4a9eb485f99e9a10df6d8f
Sairam Bisai <bisaisairam143p@gmail.com>
2026-05-14 11:45:00 +0530

    Add output for instance public IP

--- EC2/output.tf
+++ EC2/output.tf
@@ -3,7 +3,7 @@ output "instance_id" {
   value = aws_instance.my-instance.id
 }
 
-output "instance_id" {
+output "instance_public_id" {
     description = "IP of the instance"
     value = aws_instance.my-instance.public_ip
 }
