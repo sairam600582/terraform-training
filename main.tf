c823d664ecdcbc9f5f001a8afc63d60a994f0392
Sairam Bisai <bisaisairam143p@gmail.com>
2026-05-14 13:03:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
-provider "aws" {
-  region = var.region
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "MypairKey.pem"
 }
 
-module "ec2-instance" {
-  source = "./EC2-instance"
-  
+resource "aws_key_pair" "generated_key" {
+  key_name   = "terraform-key"
+  public_key = tls_private_key.generated.public_key_openssh
 }
 
 module "vpc" {
-  source = "./VPC"
+  source = "./modules/vpc"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  subnet_id         = module.vpc.subnet_id
+  security_group_id = module.vpc.security_group_id
+  key_name          = aws_key_pair.generated_key.key_name
 }
