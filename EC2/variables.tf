a3ab2b6b235e727c3b380a2838673c27eb232f59
Sairam Bisai <bisaisairam143p@gmail.com>
2026-05-14 12:56:53 +0530

    Add new variables for subnet, security group, and key name

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,11 +1,17 @@
-variable "instance" {
-  default = "t3.medium"
+variable "subnet_id" {}
+
+variable "security_group_id" {}
+
+variable "key_name" {}
+
+variable "availability_zone" {
+  default = "ap-south-1a"
 }
 
 variable "ami" {
   default = "ami-0f918f7e67a3323f0"
 }
 
-variable "availability_zone" {
-  default = "ap-south-1a"
+variable "instance" {
+  default = "t2.micro"
 }
