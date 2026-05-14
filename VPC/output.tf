c512f1d0d257b3f56fb9a980a459f4b8c0e460ab
Sairam Bisai <bisaisairam143p@gmail.com>
2026-05-14 12:57:39 +0530

    Add outputs for subnet and security group IDs

--- VPC/output.tf
+++ VPC/output.tf
@@ -1,4 +1,7 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value = aws_vpc.myvpc.id
+output "subnet_id" {
+  value = aws_subnet.public-subnet.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.sg-1.id
 }
