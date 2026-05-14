f7f172273a11261ec4fdbaf1a230bcf84bb0a295
Sairam Bisai <bisaisairam143p@gmail.com>
2026-05-14 12:56:07 +0530

    Refactor EC2 instance resource configuration

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,9 +1,13 @@
 resource "aws_instance" "my-instance" {
-  ami = var.ami
-  instance_type = var.instance
-  security_groups = [aws_security_group.sg-1.id]
-  subnet_id = aws_subnet.public-subnet.id
-  key_name = aws_key_pair.generated_key.key_name
+  ami               = var.ami
+  instance_type     = var.instance
+
+  subnet_id = var.subnet_id
+
+  vpc_security_group_ids = [var.security_group_id]
+
+  key_name = var.key_name
+
   availability_zone = var.availability_zone
 
   associate_public_ip_address = true
