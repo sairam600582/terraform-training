26970a9081c0991d38c129a1f575462f23fa0ae6
Sairam Bisai <bisaisairam143p@gmail.com>
2026-05-19 16:48:00 +0530

    Delete EC2 directory

--- EC2/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the instance"
-  value = aws_instance.my-instance.id
-}
-
-output "instance_public_id" {
-    description = "IP of the instance"
-    value = aws_instance.my-instance.public_ip
-}
